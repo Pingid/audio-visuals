d46b842206234c98310b07dd66f101f6a3692aba
Dan <dan@Macs-MacBook-Pro.local>
2018-03-15 22:07:22 +0000

    Added first project and front page

---
+++ 01-sphere/index.js
@@ -0,0 +1,34 @@
+const glsl = require('glslify');
+const regl = require('regl')()
+const camera = require('regl-camera')(regl, { 
+	distance: 1.5,
+	theta: Math.PI * .5,
+	mouse: true,
+	center: [0, 0, 0]
+})
+
+const sphere = require('./sphere');
+
+const drawSphere = sphere(regl, .5, 6, [0, 0, 0]);
+
+require('regl-audio/microphone')({
+  regl,
+  beats: 16,
+  name: '',
+  maxPitch: 1000,
+  minPitch: 500,
+  pitches: 400,
+  // beatThreshold: .1,
+  done: (microphone) => {
+
+    regl.frame(() => {	
+			microphone(({ freqTexture, timeTexture, beats, pitches, volume, cepstrum }) => {
+				camera(() => {
+					regl.clear({ color: [1, 1, 1, 1], depth: true })
+          drawSphere({  });
+					// points,point,lines,line,triangles,triangle,line loop,line strip,triangle strip,triangle fan
+				})
+			})
+		})
+  }
+})
